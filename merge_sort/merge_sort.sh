00cfa72da89b3ce33af8cbff0991ac124c63f9d3
Zahi Shaikh <zahishaikh@hotmail.com>
2026-01-22 01:14:28 -0500

    Bash stuff

--- merge_sort/merge_sort.sh
+++ merge_sort/merge_sort.sh
@@ -2,4 +2,22 @@
 #SBATCH --job-name=zahi-shaikh-merge-sort
 #SBATCH --partition=Centaurus
 #SBATCH --time=00:01:00
-./merge_sort
+make
+echo "10^1:"
+./merge_sort 10
+echo "10^2:"
+./merge_sort 100
+echo "10^3:"
+./merge_sort 1000
+echo "10^4:"
+./merge_sort 10000
+echo "10^5:"
+./merge_sort 100000
+echo "10^6:"
+./merge_sort 1000000
+echo "10^7:"
+./merge_sort 10000000
+echo "10^8:"
+./merge_sort 100000000
+echo "10^9:"
+./merge_sort 1000000000
