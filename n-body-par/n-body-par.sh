6b49c4e99dce62acb1787bf434ee51c821737624
Zahi Shaikh <zahishaikh@hotmail.com>
2026-04-07 23:27:41 -0400

    Scripting fix

--- n-body-par/n-body-par.sh
+++ n-body-par/n-body-par.sh
@@ -2,25 +2,25 @@
 #SBATCH --job-name=zahi-shaikh-n-body-par
 #SBATCH --partition=Centaurus
 #SBATCH --mem=32G
-#SBATCH --time=00:10:00
+#SBATCH --time=01:00:00
 
 make clean && make
 
 echo ""
 echo "=== Solar system (dt=200, nbstep=5000000) ==="
 for t in 1 2 4 8 16; do
-  ./nbody planet 200 5000000 5000000 $t
+  ./nbody planet 200 5000001 5000000 $t
 done
 
 echo ""
 echo "=== Random 100 particles (dt=1, nbstep=10000) ==="
 for t in 1 2 4 8 16; do
-  ./nbody 100 1 10000 10000 $t
+  ./nbody 100 1 10001 10000 $t
 done
 
 echo ""
 echo "=== Random 1000 particles (dt=1, nbstep=10000) ==="
 for t in 1 2 4 8 16; do
-  ./nbody 1000 1 10000 10000 $t
+  ./nbody 1000 1 10001 10000 $t
 done
 echo ""
